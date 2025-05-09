b24b3876e7f6348bd38dcf387882df9eefa2c9c3
Junk Pixel <never2nv@junkpixel.net>
2025-05-09 11:42:46 -0400

    1.13.1 - Update to MC 1.21.5
    Updated to 1.21.5, next we'll address some serious bugs... hopefully.

--- src/test/java/dev/drawethree/xprison/utils/LocationUtilsTest.java
+++ src/test/java/dev/drawethree/xprison/utils/LocationUtilsTest.java
@@ -2,9 +2,7 @@ package dev.drawethree.xprison.utils;
 
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 
 import static dev.drawethree.xprison.utils.location.LocationUtils.INVALID_LOCATION;
 import static dev.drawethree.xprison.utils.location.LocationUtils.toXYZW;
@@ -12,49 +10,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-
 public class LocationUtilsTest {
-
-
-	@Mock
-	private Location mockLocation;
-
-	@Mock
-	private World mockWorld;
-
-	@BeforeEach
-	public void setUp() {
-
-	}
 
 	@Test
 	public void test_success_toXYZW_0_0_0_world() {
 
-		this.mockWorld = mock(World.class);
-		this.mockLocation = mock(Location.class);
+		World mockWorld = mock(World.class);
+		when(mockWorld.getName()).thenReturn("world");
+
+		Location location = new Location(mockWorld, 0, 0, 0);
 
 		String expected = "X: 0, Y: 0, Z: 0, World: world";
-
-		when(mockWorld.getName()).thenReturn("world");
-		when(mockLocation.getWorld()).thenReturn(mockWorld);
-		when(mockLocation.getBlockX()).thenReturn(0);
-		when(mockLocation.getBlockY()).thenReturn(0);
-		when(mockLocation.getBlockZ()).thenReturn(0);
-
-		String actual = toXYZW(mockLocation);
+		String actual = toXYZW(location);
 
 		assertEquals(expected, actual);
-
 	}
 
 	@Test
 	public void test_sucecss_toXYZW_null() {
 
-		this.mockLocation = null;
+		Location location = null;
 
-		String actual = toXYZW(mockLocation);
+		String actual = toXYZW(location);
 
 		assertEquals(INVALID_LOCATION, actual);
-
 	}
 }
--- src/test/java/dev/drawethree/xprison/utils/misc/NumberUtilsTest.java
+++ src/test/java/dev/drawethree/xprison/utils/misc/NumberUtilsTest.java
@@ -1,9 +1,8 @@
 package dev.drawethree.xprison.utils.misc;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NumberUtilsTest {
 
